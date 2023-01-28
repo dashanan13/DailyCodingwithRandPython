9a9170828a172d0de2fdd5aa0dc903cd9c835f1f
Mohit <mohit13@outlook.com>
2023-01-28 01:08:09 +0000

    Environment setup details updated

--- Day1/R/temp.R
+++ Day1/R/temp.R
@@ -1,11 +1,11 @@
-vectr1 <- c(1,2,3,4,5)
-vectr2 <- c(6,7,8)
-vectr3 <- c(5,4,3,2,1)
-vectr4 <- c(1,2,3)
+vectr1 <- list(1,2,3,4,5)
+vectr2 <- list(6,7,8)
+vectr3 <- list(5,4,3,2,1)
+vectr4 <- list(1,2,3)
 clist1 <-list(vectr1,vectr2,vectr3)
-append(clist1, vectr4) 
+#append(clist1, vectr4) 
 
 
-print(clist1)
+array(clist1)
 
 #sprintf("Need a 2 number combination that sums upto %2d from the list %2s", vectr)
